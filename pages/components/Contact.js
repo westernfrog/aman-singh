961d33d60e0b713e5937182a8b8c8dee6e170bb2
Aman Singh <haaamansingh007@gmail.com>
2023-01-07 13:54:42 +0530

    Updated

--- pages/components/Contact.js
+++ pages/components/Contact.js
@@ -3,7 +3,7 @@ export default function Contact() {
     <>
       <div className="container">
         <button
-          className="btn btn-dark btn-lg fixed-bottom mx-lg-5 my-lg-5 rounded-circle mx-3 my-5"
+          className="btn btn-dark btn-lg fixed-bottom mx-lg-5 my-lg-5 rounded-pill mx-3 my-5"
           style={{
             left: "auto",
             boxShadow: "inherit",
@@ -22,27 +22,25 @@ export default function Contact() {
           id="offcanvasRight"
           aria-labelledby="offcanvasRightLabel"
         >
-          <div className="offcanvas-header bg-dark text-light">
+          <div className="offcanvas-header bg-dark text-light rounded-3 shadow-sm">
             <h5 id="offcanvasRightLabel">
               <div class="list-group">
                 <a
                   href="#"
-                  class="list-group-item list-group-item-action d-flex gap-3 py-3"
+                  class="list-group-item list-group-item-action d-flex gap-3 py-3 bg-dark border-0"
                   aria-current="true"
                 >
                   <img
-                    src="https://github.com/twbs.png"
+                    src="https://cdn-icons-png.flaticon.com/512/185/185788.png"
                     alt="twbs"
                     width="32"
                     height="32"
                     class="rounded-circle flex-shrink-0"
                   />
-                  <div class="d-flex gap-2 w-100 justify-content-between">
+                  <div class="d-flex gap-2 w-100 justify-content-between bg-dark text-light">
                     <div>
-                      <h6 class="mb-0">List group item heading</h6>
-                      <p class="mb-0 opacity-75">
-                        Some placeholder content in a paragraph.
-                      </p>
+                      <h6 class="mb-0 text-rubik">Amswe Bot</h6>
+                      <p class="mb-0 opacity-75 h6 text-dm">Ask me anything!</p>
                     </div>
                   </div>
                 </a>
