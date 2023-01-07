860d3bc322f5fe1a2a79407e4b2a7ad0d378c366
Aman Singh <haaamansingh007@gmail.com>
2023-01-07 11:02:58 +0530

    Updated

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -2,10 +2,10 @@ export default function Footer(params) {
   return (
     <>
       <div className="container vw-100 vh-100 d-flex align-items-center justify-content-between">
-        <div className="text-light text-mono mx-auto text-center">
+        <div className="text-light text-mono mx-auto text-center rose py-5">
           <h1 className="display-4 fw-bold">
             Lets get in&nbsp;
-            <span className="fw-bold" style={{ color: "maroon" }}>
+            <span className="fw-bold" style={{ color: "#FF0032" }}>
               touch!
             </span>
           </h1>
--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -6,22 +6,22 @@ export default function Projects() {
       <Project
         title="Carnet"
         lang="Next.js and MongoDB"
-        img="https://cdn-icons-png.flaticon.com/512/4844/4844359.png"
+        img="https://cdn-icons-png.flaticon.com/512/9213/9213476.png"
       />
       <Project
         title="Utilisauras"
         lang="Next.js and MongoDB"
-        img="https://cdn-icons-png.flaticon.com/512/4844/4844359.png"
+        img="https://cdn-icons-png.flaticon.com/512/136/136522.png"
       />
       <Project
         title="SongBird"
         lang="React"
-        img="https://cdn-icons-png.flaticon.com/512/4844/4844359.png"
+        img="https://cdn-icons-png.flaticon.com/512/2400/2400811.png"
       />
       <Project
         title="Planets"
         lang="HTML and CSS"
-        img="https://cdn-icons-png.flaticon.com/512/4844/4844359.png"
+        img="https://cdn-icons-png.flaticon.com/512/2407/2407423.png"
       />
     </>
   );
